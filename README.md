9b93cd9e914eb019318a97aded04fd8f78c99114
Hilary Lamb <hilaryklamb@gmail.com>
2019-09-09 17:24:48 -0400

    initial html, trivia questions added to js, jumbotron and start button boostrap

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
 # TriviaGame
+
+Create a trivia form with true/false options
+Player will have a limited amount of time to finish quiz
+Game ends when time runs out
+Page will reveal number of questions that players answer correctly and incorrectly
+Player can’t pick more than one answer per question
+Include countdown timer
+
+Timers 
+Debugging
+JS / Jquery review
+setTimeout
+setInterval
+
+Pseudo code
+
+Html bootstrap elements
+Jumbotron
+Header “Trivia Game!”
+Start button
+
+Timer begins
+Trivia questions are listed
+Create questions as variables to be looped through
+
+
+Functions:
+Start: on click
+Timer: setInterval
+True or false
+Score counter:
+Correct answers
+Incorrect answers
+unanswered
+Alert all done
+
+Reset
+
+
+Trivia questions:
+
+What was the average life expectancy of white males born in the U.S. just before the Civil War?
+A. 40 years
+B. 50 years
+C. 60 years
+D. 70 years
+
+What is a shaddock?
+A. a fish, the offspring of a male shad and a female haddock
+B. a crystal, such as quartz, that sticks out from a mineral vein
+C. a plant that is a member of the nightshade family
+D. a grapefruit
+ 
+Which one of the following instruments is used to measure humidity?
+A. anemometer
+B. ammeter
+C. hygrometer
+D. barometer
+
+Which two planets are most similar in size diameter wise?
+A. Mars and Mercury
+B. Venus and Earth
+C. Uranus and Neptune
+D. Jupiter and Saturn
+ 
+If a hertz is equal to one cylce per second, how manyh cycles per second does a megahertz  equal?
+A. 1/1,000
+B. 1,000
+C. 1,000,000
+D. 1,000,000,000
+ 
+What principle explains why cold food warms up and hot food cools off when stored at room temperature?
+A. entropy
+B. chemical equilibrium
+C. momentum
+D. relativity
+ 
+Which color is not considered to be one of the primary colors of light?
+A. Red
+B. yellow
+C. green
+D. blue
+ 
+What causes the disease toxoplasmosis?
+A. A bacterium
+B. a protozoan
+C. a virus
+D.  a prion
+ 
+What is the slowest wind speed a hurricane can have according to the Saffir-Simpson scale?
+A. 50 m.p.h.
+B. 74 m.p.h.
+C. 96 m.p.h.
+D. 110 m.p.h.
+
+Which of the following heavenly bodies have never had a spacecraft landed on it?
+A. Venus
+B. Mars
+C. the moon
+D. Jupiter
+
+A. 40 years
+D. a grapefruit
+C. hygrometer
+B. Venus and Earth
+C. 1,000,000
+A. entropy
+B. yellow
+B. a protozoan
+B. 74 m.p.h.
+D. Jupiter
+
+
+
+
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,102 @@
+var questions = [
+    {
+      question: "What was the average life expectancy of white males born in the U.S. just before the Civil War?",
+      answers: {
+        a: "40 years",
+        b: "50 years",
+        c: "60 years",
+        d: "70 years",
+      },
+      correctAnswer: "a"
+    },
+    {
+        question: "What is a shaddock?",
+        answers: {
+          a: "a fish, the offspring of a male shad and a female haddock",
+          b: "a crystal, such as quartz, that sticks out from a mineral vein",
+          c: "a plant that is a member of the nightshade family",
+          d: "a grapefruit",
+        },
+        correctAnswer: "d"
+      },
+      {
+        question: "Which one of the following instruments is used to measure humidity?",
+        answers: {
+          a: "anemometer",
+          b: "ammeter",
+          c: "hygrometer",
+          d: "barometer",
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Which two planets are most similar in size diameter wise?",
+        answers: {
+          a: "Mars and Mercury",
+          b: "Venus and Earth",
+          c: "Uranus and Neptune",
+          d: "Jupiter and Saturn",
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "If a hertz is equal to one cylce per second, how manyh cycles per second does a megahertz  equal?",
+        answers: {
+          a: "1/1,000",
+          b: "1,000",
+          c: "1,000,000",
+          d: "1,000,000,000",
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "What principle explains why cold food warms up and hot food cools off when stored at room temperature?",
+        answers: {
+          a: "entropy",
+          b: "chemical equilibrium",
+          c: "momentum",
+          d: "relativity",
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Which color is not considered to be one of the primary colors of light?",
+        answers: {
+          a: "Red",
+          b: "Yellow",
+          c: "Green",
+          d: "Blue",
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "What causes the disease toxoplasmosis?",
+        answers: {
+          a: "A bacterium",
+          b: "a protozoan",
+          c: "a virus",
+          d: "a prion",
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "What is the slowest wind speed a hurricane can have according to the Saffir-Simpson scale?",
+        answers: {
+          a: "50 m.p.h.",
+          b: "74 m.p.h.",
+          c: "96 m.p.h.",
+          d: "110 m.p.h.",
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Which of the following heavenly bodies have never had a spacecraft landed on it?",
+        answers: {
+          a: "Venus",
+          b: "Mars",
+          c: "the moon",
+          d: "Jupiter",
+        },
+        correctAnswer: "d"
+      },
+]
